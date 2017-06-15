af03bd5a02402bd90b4a64a358e723a05d5f732d
刘东旭 <ldongxu@126.com>
2017-06-15 11:28:50 +0800

    typesafe.config

--- src/main/java/com/ldongxu/App.java
+++ src/main/java/com/ldongxu/App.java
@@ -1,10 +1,15 @@
 package com.ldongxu;
 
 
+import com.ldongxu.learn.CloneUtils;
+import com.ldongxu.learn.ConfigUtil;
 import com.ldongxu.learn.Person;
 
-import java.lang.reflect.Method;
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Hello world!
@@ -13,12 +18,63 @@ import java.lang.reflect.Method;
 public class App 
 {
     public static void main(String[] args) {
+//        System.out.println("java_vendor:" + System.getProperty("java.vendor"));
+//        System.out.println("java_vendor_url:"
+//                + System.getProperty("java.vendor.url"));
+//        System.out.println("java_home:" + System.getProperty("java.home"));
+//        System.out.println("java_class_version:"
+//                + System.getProperty("java.class.version"));
+//        System.out.println("java_class_path:"
+//                + System.getProperty("java.class.path"));
+//        System.out.println("os_name:" + System.getProperty("os.name"));
+//        System.out.println("os_arch:" + System.getProperty("os.arch"));
+//        System.out.println("os_version:" + System.getProperty("os.version"));
+//        System.out.println("user_name:" + System.getProperty("user.name"));
+//        System.out.println("user_home:" + System.getProperty("user.home"));
+//        System.out.println("user_dir:" + System.getProperty("user.dir"));
+//        System.out.println("java_vm_specification_version:"
+//                + System.getProperty("java.vm.specification.version"));
+//        System.out.println("java_vm_specification_vendor:"
+//                + System.getProperty("java.vm.specification.vendor"));
+//        System.out.println("java_vm_specification_name:"
+//                + System.getProperty("java.vm.specification.name"));
+//        System.out.println("java_vm_version:"
+//                + System.getProperty("java.vm.version"));
+//        System.out.println("java_vm_vendor:"
+//                + System.getProperty("java.vm.vendor"));
+//        System.out
+//                .println("java_vm_name:" + System.getProperty("java.vm.name"));
+//        System.out.println("java_ext_dirs:"
+//                + System.getProperty("java.ext.dirs"));
+//        System.out.println("file_separator:"
+//                + System.getProperty("file.separator"));
+//        System.out.println("path_separator:"
+//                + System.getProperty("path.separator"));
+//        System.out.println("line_separator:"
+//                + System.getProperty("line.separator"));
+//        System.out.println("j" +
+//                "ava.io.tmpdir:"+System.getProperty("java.io.tmpdir"));
+//        System.out.println("java.compiler:"+System.getProperty("java.compiler"));
+//        System.out.println("java.ext.dirs:"+System.getProperty("java.ext.dirs"));
+//        System.out.println("java.class.path:"+System.getProperty("java.class.path"));
+//        System.out.println("java.library.path:"+System.getProperty("java.library.path"));
+//
+//        System.out.println("===========================");
+//
+//        System.out.println(System.getenv());
 
-        Method[] methods = Person.class.getMethods();
-        for (Method method:methods){
-            System.out.println(method.getName());
-            System.out.println(method.getParameterCount());
+        System.out.println(0x000000);
+
+        List<Person> list = new ArrayList<>();
+        for (int i=0;i<100;i++){
+            Person person = new Person();
+            person.setName("test"+i);
+            list.add(person);
         }
+        List<Person> list1 = new ArrayList<>(list);
+       list.get(0).setName("hhhh");
+        System.out.println(list1.size());
+
     }
 
 
---
+++ src/main/java/com/ldongxu/learn/ConfigUtil.java
@@ -0,0 +1,25 @@
+package com.ldongxu.learn;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * Created by 刘东旭 on 2017/6/7.
+ */
+public class ConfigUtil {
+    private final static Config config;
+
+    static {
+        config = ConfigFactory.load();
+    }
+
+    public static String getString(String var){
+        return config.getString(var);
+    }
+
+    public static Config getConfig(){
+        return config;
+    }
+
+
+}
